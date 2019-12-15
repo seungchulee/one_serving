ad0420ce4629aa4f3ed1b7b2a50efd3ab300b7be
Ubuntu <ubuntu@ip-172-31-37-194.us-east-2.compute.internal>
2019-12-15 07:10:32 +0000

    modify login.html

--- server/main.js
+++ server/main.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    var html = fs.readFile('./static/login.html', function(err, html){
+        html = "" + html;
+        if(err) throw err;
+        res.writeHeader(200, {"Content-Type":"text/html"});
+        res.write(html);
+        res.end();
+    });
 });
 
 app.listen(3000, function(){
